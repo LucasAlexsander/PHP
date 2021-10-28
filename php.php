38af5480ef941199631b093635aac6e2903f8adb
Lucas Alexsander Barbosa Cruz <90354669+LucasAlexsander@users.noreply.github.com>
2021-10-28 12:47:23 -0300

    Coisas Novas!

--- php.php
+++ php.php
@@ -1,3 +1,4 @@
+
 <?php
 $x= 5;
 $y= 10;
@@ -164,4 +165,139 @@ echo "<br><hr>";
 define("OLA", "Bem-vindo ao meu site!");
 echo OLA;
 echo "<br><hr>";
+?>
+
+<?php
+$x = 10;
+$y = 2;
+if ($x+$y = 12) {
+    echo "A soma é igual a 12.";
+    echo "<br><hr>";
+}
+?>
+
+
+<?php
+$x = 0;
+while ($x <= 5) {
+    echo "O número é: $x <br>";
+    $x++;
+}
+echo "<hr>";
+?>
+
+<?php
+for ($x=0; $x<5; $x++) {
+    if ($x == 4) {
+        break;
+    }
+    echo "O número é: $x <br>";
+}
+echo "<hr>";
+?>
+
+<?php
+for ($x=0; $x < 10; $x++) {
+    if ($x == 4) {
+        continue;
+    }
+    echo "O número é: $x <br>";
+}
+echo "Não vai aparecer o número 4. <br><hr>";
+?>
+
+<?php
+function msgEscrita () {
+    echo "Olá, mundo!";
+}
+
+msgEscrita();
+echo "<br><hr>";
+?>
+
+<?php
+function familyName($fname) {
+    echo "$fname Barbosa <br>";
+}
+familyName("Lucas");
+familyName("Renata");
+familyName("Maria das Graças");
+echo "<hr>";
+?>
+
+<?php
+function nameAge ($fname, $age) {
+    echo "$fname tem $age anos. <br>";
+}
+nameAge("Lucas", "19");
+nameAge("Renata", "49");
+nameAge("Maria das Graças", "63");
+echo "<hr>";
+?>
+
+<?php
+function setHeight(int $minheight = 50) {
+    echo "A altura é: $minheight <br>";
+}
+setHeight(350);
+setHeight(100);
+setHeight(); //Vai aparecer 50, pois foi defindo como padrão.
+setHeight(893);
+echo "<hr>";
+?>
+
+<?php
+function soma ($x, $y) {
+    $z = $x + $y;
+    return $z;
+}
+echo "5 + 10 = " .soma(5, 10). "<br>";
+echo "6 + 52 = " .soma(6, 52). "<br>";
+echo "<hr>"
+?>
+
+<?php
+function add_five (&$valor) {
+    $valor += 5;
+}
+$num = 2;
+add_five($num);
+echo "$num <br><hr>";
+?>
+
+<?php
+$cars = array("Volvo", "BWM", "Toyota");
+echo count($cars);
+echo "<hr>";
+?>
+
+<?php
+$cars = array("Volvo", "BMW", "Toyota");
+$arrlegnth = count($cars); //Conta quantos carros tem
+for ($x = 0; $x < $arrlegnth; $x++) {
+    echo $cars[$x], "<br>";
+}   
+echo "<hr>";
+?>
+
+<?php
+$str = "Visit W3Schools";
+$pattern = "/w3schools/i";
+echo "Novo!!!<br>";
+echo preg_match($pattern, $str);
+echo "<br><hr>";
+?>
+
+
+<?php
+$str = "The rain in SPAIN falls mainly on the plains.";
+$pattern = "/ain/i";
+echo preg_match_all($pattern, $str);
+echo "<br><hr>";
+?>
+
+<?php
+$str = "Visit Microsoft!";
+$pattern = "/microsoft/i";
+echo preg_replace($pattern, "W3Schools", $str); // Outputs "Visit W3Schools
 ?>

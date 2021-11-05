35766db534a91372b834493e614ae118b254f07d
Lucas Alexsander Barbosa Cruz <90354669+LucasAlexsander@users.noreply.github.com>
2021-11-05 12:46:36 -0300

    Agora foi

--- upload/uploads.php
+++ upload/uploads.php
@@ -2,12 +2,14 @@
 <html>
 <body>
     <?php
-        $target_dir = "uploads/";
+        $target_dir =   getcwd(). DIRECTORY_SEPARATOR; 
         $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
         $uploadOk = 1;
         $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
         //Checando se a imagem é uma imagem real ou falsa
         if (isset($_POST["submit"])) {
+
+
             $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
             if ($check !== false) {
                 echo "Arquivo é uma imagem - ". $check["mime"];
@@ -33,16 +35,17 @@
             $uploadOk = 0;
         }
         //Checando se o uplaodOk é 0 por um erro
-        /*if ($uploadOk == 0) {
+        if ($uploadOk == 0) {
             echo "Desculpe, seu arquivo não foi carrgado.";
         //Se tudo estiver certo, tente carregar o arquivo
         } else {
+ 
             if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
                 echo "O arquivo ". htmlspecialchars(basename ($_FILES["fileToUpload"]["name"])). " foi enviado para o carregamneto.";  
             } else {
                 echo "Desculpe, erro no carregamento do arquivo.";
             }
-        }*/
+        }
     ?>
 </body>
 </html>
